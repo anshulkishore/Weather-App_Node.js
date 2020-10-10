f979136614795f96b6159d3b7edab7818f7b790b
Anshul Kishore <anshulkishore310@gmail.com>
2020-10-10 18:25:24 +0530

    accessing other fields from weatherstack response json

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('postman-request')
 
-const url = 'http://api.weatherstack.com/current?access_key=166e3027b4d07ae8a47207559cb6c2cc&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=166e3027b4d07ae8a47207559cb6c2cc&query=37.8267,-122.4233&units=m'
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (error, response) => {
+    console.log(response.body.current)
+    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out.')
 })
