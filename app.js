60186cfbd0f52613321aa28f30fca07da0ea2756
Anshul Kishore <anshulkishore310@gmail.com>
2020-10-10 19:54:07 +0530

    error handling with weatherstock and mapbox service

--- app.js
+++ app.js
@@ -4,14 +4,27 @@ const urlWeatherStack = 'http://api.weatherstack.com/current?access_key=166e3027
 const geoCodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5zaHVsa2lzaG9yZSIsImEiOiJja2czbnkzMncwYnFnMndwamhiOThvaTdmIn0.xxpTwBdBJ65MP9Jnu4bMrA'
 
 request({url: urlWeatherStack, json: true}, (error, response) => {
-    // console.log(response.body.current)
-    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out.')
+    if (error) {
+        console.log('Unable to connect to weather service !')
+    } else if (response.body.error) {
+        console.log('Unable to find location !')
+    } else {
+        console.log(response.body.current.weather_descriptions[0] + '. It is currently ' 
+        + response.body.current.temperature + ' degrees out.')    
+    }
 })
 
 //GeoCoding
 //Address -> Lat/Long -> Weather
+
 request({url: geoCodeURL, json: true}, (error, response) => {
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
-    console.log(latitude, longitude)
+    if (error) {
+        console.log('Unable to connect to geocoding service !')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find geocode. Try another search.')
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
 })
