51635e3b5560d888bec1e471f0f4c366369e6d27
Anshul Kishore <anshulkishore310@gmail.com>
2020-10-12 21:31:45 +0530

    Started implementing callback functions

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
 const request = require('postman-request')
+const geocode = require('./utils/geocode')
 
-const urlWeatherStack = 'http://api.weatherstack.com/current?access_key=166e3027b4d07ae8a47207559cb6c2cc&query=37.8267,-122.4233&units=m'
-const geoCodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5zaHVsa2lzaG9yZSIsImEiOiJja2czbnkzMncwYnFnMndwamhiOThvaTdmIn0.xxpTwBdBJ65MP9Jnu4bMrA'
+// const urlWeatherStack = 'http://api.weatherstack.com/current?access_key=166e3027b4d07ae8a47207559cb6c2cc&query=37.8267,-122.4233&units=m'
+// const geoCodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5zaHVsa2lzaG9yZSIsImEiOiJja2czbnkzMncwYnFnMndwamhiOThvaTdmIn0.xxpTwBdBJ65MP9Jnu4bMrA'
 
-request({url: urlWeatherStack, json: true}, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to weather service !')
-    } else if (response.body.error) {
-        console.log('Unable to find location !')
-    } else {
-        console.log(response.body.current.weather_descriptions[0] + '. It is currently ' 
-        + response.body.current.temperature + ' degrees out.')    
-    }
-})
+// request({url: urlWeatherStack, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service !')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location !')
+//     } else {
+//         console.log(response.body.current.weather_descriptions[0] + '. It is currently ' 
+//         + response.body.current.temperature + ' degrees out.')    
+//     }
+// })
 
-//GeoCoding
-//Address -> Lat/Long -> Weather
-
-request({url: geoCodeURL, json: true}, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to geocoding service !')
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find geocode. Try another search.')
-    } else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)
-    }
+geocode('Boston', (error, data) => {
+    console.log('Error: ', error)
+    console.log('Data: ', data)
 })
